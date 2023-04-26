2a736a36f3b23fdef9dbb2cdf1181a8ed24b6759
Alexandr Neveroff <neveroff1976@gmail.com>
2023-04-26 21:02:24 +0300

    Stats w/o styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import { Profile } from '../Profile/Profile';
+import { Statistics } from '../Statistics/Statistics';
 import { Container } from './App.styled';
 import user from 'data/user.json';
+import stats from 'data/stats.json';
 
 export const App = () => {
   return (
@@ -12,6 +14,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={stats} />
+      <Statistics stats={stats} />
     </Container>
   );
 };
